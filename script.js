50a2532109a85c79c6417a5761bd29a5059dc621
Jennifer <32855060+jennnico@users.noreply.github.com>
2018-08-21 18:23:16 -0400

    Tooltip added, including fade-in effect.

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
+//Resource for formatting style of title: http://bl.ocks.org/d3noob/a22c42db65eb00d4e369
+
 document.addEventListener('DOMContentLoaded',function(){
   req=new XMLHttpRequest();
   req.open("GET",'https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/cyclist-data.json',true);
   req.send();
   req.onload=function(){
     json=JSON.parse(req.responseText);
-       
+    
     //Data array that WILL store year and time (in SECONDS)
     var data = []
     //array of racetimes, split into arrays of min and sec
@@ -13,12 +15,13 @@ document.addEventListener('DOMContentLoaded',function(){
       data.push([val.Year, val.Time, val.Name, val.Nationality, val.Doping])
       times.push(val.Time.split(":"))
     })
-  
+
   //turn racetimes into SECONDS, and add to data array
    for(var i=0; i<data.length; i++){
       data[i].push((times[i][0]*60 + Number(times[i][1])))
     }
     
+    //Set dimensions of the canvas
     const w = 800;
     const h = 500;
     const padding = 50;
@@ -33,15 +36,23 @@ document.addEventListener('DOMContentLoaded',function(){
                      .domain([d3.max(data, d => d[5]), d3.min(data, d => d[5])])
                      .range([h-padding, padding])
     
-    //Set the X and Y axes
+    //Define the axes
     const xAxis = d3.axisBottom(xScale)
     const yAxis = d3.axisLeft(yScale)
     
-    const svg = d3.select("body")
+    // Define the div for the tooltip
+    var div = d3.select("a").append("div")	
+                .attr("class", "tooltip")				
+                .style("opacity", 0);
+    
+    // Adds the svg canvas
+    const svg = d3.select("a")
                   .append("svg")
                   .attr("width", w)
                   .attr("height", h);
     
+
+    // Add the scatterplot
     svg.selectAll("circle")
        .data(data)
        .enter()
@@ -52,12 +63,25 @@ document.addEventListener('DOMContentLoaded',function(){
        .attr("class", "circle")
        .style('fill', (d) => {
           return (d[4] ? "blue" : "orange")
-        })
-       .append("title")
-       .text(d => d[2] + ": " + d[3] + " Year: " + d[0] + " Time: " + d[1] + d[4])
+       })
+        //Original tooltips... could not be formatted easily
+        // .append("title")
+       //.text(d => d[2] + ": " + d[3] + " \nYear: " + d[0] + " Time: " + d[1] + "\n" + d[4])
+       .on("mouseover", function(d) {		
+           div.transition()		
+               .duration(200)		
+               .style("opacity", .9);		
+           div .html(d[2] + ": " + d[3] + " <br/>Year: " + d[0] + " Time: " + d[1] + "<br/>" + d[4])	
+               .style("left", (d3.event.pageX + 10) + "px")		
+               .style("top", (d3.event.pageY - 28) + "px");
+       })					
+       .on("mouseout", function(d) {		
+           div.transition()		
+               .duration(500)		
+               .style("opacity", 0);	
+       });
     
-    
-    //move the X-axis
+    //Add and move the X-axis
     svg.append("g")
        .attr("transform", "translate(0, " + (h-padding) + ")")
        .call(xAxis)
@@ -67,10 +91,8 @@ document.addEventListener('DOMContentLoaded',function(){
        .attr("x", w/2)
        .attr("y", padding-2)
        .text("Year of Race")
-    
-    
-    
-    //move the Y-axis
+     
+    //Add and move the Y-axis
     svg.append("g")
        .attr("transform", "translate(" + (padding + 10) + ", 0)")
        .call(yAxis)
